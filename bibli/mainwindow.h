9bb5ff3906d93fa60de489254a2bb3f2061afb39
Miarimanana <nrandriamiarimanana@gmail.com>
2024-09-20 17:56:15 +0300

    Version 1 Miary

---
+++ bibli/mainwindow.h
@@ -0,0 +1,57 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+
+#include <QtCharts>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btnDash_clicked();
+
+    void on_btnMember_clicked();
+
+    void on_btnBook_clicked();
+
+    void on_btnEmprunt_clicked();
+
+    void on_btnCommunication_clicked();
+
+    void on_btnSetting_clicked();
+
+    void on_btnAbout_clicked();
+
+    void on_btnHelp_clicked();
+
+    void on_btnAccueil_clicked();
+
+    /*===================*/
+    void updateChart();
+    /*===================*/
+
+
+private:
+    Ui::MainWindow *ui;
+
+    QSqlDatabase db;
+    QSqlQuery query;
+
+    QChart *m_chart;
+
+};
+#endif // MAINWINDOW_H
